9ca0370426268e51376f2de09ca9ff7baa7b6940
SAHIL-NAZIR <sahilnazirlone26@gmail.com>
2021-03-05 23:09:25 +0530

    update

---
+++ Area and perimeter of Rectangle.c
@@ -0,0 +1,16 @@
+                                                        /**C program for Area and perimeter of Rectangle**/
+
+    #include<stdio.h>
+    main()
+    {
+        float a,b;
+        printf("Enter length of rect.:  ");
+        scanf("%f",&a);
+        printf("Enter breadth of rect.:  ");
+        scanf("%f",&b);
+        printf("Area = %.2f\n",a*b);
+        printf("Perimeter = %.2f",2*(a+b));
+}
+
+
+
