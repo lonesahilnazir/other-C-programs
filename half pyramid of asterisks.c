960fae5a3573fea009e180eedba4c68d9772abb5
SAHIL-NAZIR <sahilnazirlone26@gmail.com>
2021-03-07 19:50:57 +0530

    update

--- half pyramid of asterisks.c
+++ half pyramid of asterisks.c
@@ -1,12 +1,12 @@
+                                                            /**C program to print half pyramid of asterisks**/
 #include<stdio.h>
 main()
 {
-    int row,i,j;
+    int row,i,j;        //i represents i'th row,  j represents no. of * in i'th row
     printf("Enter number of rows: ");
     scanf("%d",&row);
     for(i=1;i<=row;++i)
     {
-
         for(j=1;j<=i;j++)
         {
             printf("* ");
