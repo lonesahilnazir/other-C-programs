40892884628cba5f7c5702ad5a1ff0028960fe8b
SAHIL-NAZIR <sahilnazirlone26@gmail.com>
2021-03-08 20:57:08 +0530

    adding half pyramid of integers

---
+++ half pyramid of integers.c
@@ -0,0 +1,16 @@
+                                                            /**C program to print half pyramid of integers**/
+#include<stdio.h>
+main()
+{
+    int row,i,j;        //i represents i'th row,  j represents no. of integers in i'th row
+    printf("Enter number of rows: ");
+    scanf("%d",&row);
+    for(i=1;i<=row;++i)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("%i ",j);
+        }
+        printf("\n");
+    }
+}
