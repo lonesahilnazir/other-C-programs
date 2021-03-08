36af7d700261fd2d7dce76dbbae10ad4110bd4c0
SAHIL-NAZIR <sahilnazirlone26@gmail.com>
2021-03-08 21:50:42 +0530

    adding full pyramid of asterisks

---
+++ full pyramid of asterisks.c
@@ -0,0 +1,20 @@
+                                                            /**C program to print full pyramid of integers**/
+#include<stdio.h>
+main()
+{
+    int row,i,j,s;        //s is for spaces
+    printf("Enter number of rows: ");
+    scanf("%d",&row);
+    for(i=1;i<=row;i=i+2)
+    {
+        for(s=row-i;s>=1;s=s-2)
+        {
+            printf("  ");
+        }
+        for(j=1;j<=i;j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+}
